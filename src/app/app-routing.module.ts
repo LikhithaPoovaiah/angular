e23e8907a0d5458c4cca1427a601cd8cff9a3b2b
likhithatp <likhithatp29@gmail.com>
2025-08-05 14:54:46 +0530

    angular commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{HomeComponent}from './home/home.component';
+import {UserComponent} from './user/user.component';
+import {CategoriesComponent} from './categories/categories.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'user',component:UserComponent},
+  {path:'categories', component:CategoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
